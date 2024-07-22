035350468369821311bf81278f803fe0022a08cf
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-22 07:49:21 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
